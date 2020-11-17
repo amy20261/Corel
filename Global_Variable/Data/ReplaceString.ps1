46ad219b561a72a0a7cc29034038b18f8a2babce
CORELCORP\amy.hsieh <amy.hsieh@corel.com>
2020-11-16 23:26:39 -0500

    update for test data and test scripts 1

--- Global_Variable/Data/ReplaceString.ps1
+++ Global_Variable/Data/ReplaceString.ps1
@@ -1,4 +1,4 @@
 Get-ChildItem 'C:\Corel\Global_Variable\Data\*.CSV' -Recurse | ForEach {
-(Get-Content $_ | ForEach { $_ -replace 'CDGS2019', 'CDGS2020' }) |
+(Get-Content $_ | ForEach { $_ -replace 'ESDPSP2020', 'ESDPSP2021' }) |
 Set-Content $_
 }
